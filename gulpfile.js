faa3f8b1d521e9336249b7a4871c5209ce45c35d
Aleksandr Shoronov <alexshoronov@gmail.com>
2018-06-09 00:19:22 +0300

    Initial release

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const gulp = require('gulp');
+const pug  = require('gulp-pug');
+const csso = require('gulp-csso');
+const del  = require('del');
+const bs   = require('browser-sync').create();
+
+gulp.task('pages', () => {
+	return gulp.src([
+		'src/**/*.pug',
+		'!src/_base/**/*.pug'
+	])
+		.pipe(pug())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('public', () => {
+	return gulp.src('public/**/*')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', () => {
+	return gulp.src('src/**/*.css')
+		.pipe(csso())
+		.pipe(gulp.dest('./dist'))
+		.pipe(bs.stream());
+
+});
+
+gulp.task('clean', (cb) => {
+	return del(['dist/'], cb);
+});
+
+gulp.task('build', gulp.parallel(
+	'pages',
+	'public',
+	'styles'
+));
+
+gulp.task('watch', () => {
+	bs.init({
+		notify: false,
+		open: false,
+		server: {
+			baseDir: './dist'
+		}
+	});
+
+	gulp.watch('src/**/*.pug', gulp.series('pages'));
+	gulp.watch('src/**/*.css', gulp.series('styles'));
+	gulp.watch('dist/*.html').on('change', bs.reload);
+});
+
+gulp.task('default', gulp.series(
+	'clean',
+	'build'
+));
+
+gulp.task('serve', gulp.series(
+	'default',
+	'watch'
+));
