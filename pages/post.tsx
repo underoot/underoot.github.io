dcde9f1cb7bf9b3528ab47b5c05167eb6b6ad019
Aleksandr Shoronov <alexshoronov@gmail.com>
2021-05-11 22:51:28 +0300

    Fixed image to post

--- pages/post.tsx
+++ pages/post.tsx
@@ -23,14 +23,14 @@ export function Post({
       <>
         <meta name="description" content={description} />
         <meta name="author" content="Aleksandr Shoronov" />
-        <meta name="og:image" content={image} />
+        <meta name="og:author" content="Aleksandr Shoronov" />
+        <meta name="og:image" content={`https://underoot.dev${image}`} />
         <meta name="og:image:width" content="1200" />
         <meta name="og:image:height" content="800" />
         <meta name="og:description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={`https://underoot.dev${image}} />
+        <meta name="twitter:image" content={`https://underoot.dev${image}`} />
         <meta name="twitter:title" content={title} />
-        <meta name="og:author" content="Aleksandr Shoronov" />
         <title>{title}</title>
       </>
     }>
