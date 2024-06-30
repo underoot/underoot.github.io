084cbf49f417f06826ded96418b2e495fa031f64
Aleksandr Shoronov <alexshoronov@gmail.com>
2024-06-30 17:46:59 +0300

    Added displaying of net host

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,9 @@ const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.setServerOptions({
+		showAllHosts: true
+	});
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
