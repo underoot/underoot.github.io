11c4f775a5596fde0a9727d821eda949c9c6488c
Aleksandr Shoronov <alexshoronov@gmail.com>
2026-04-12 16:33:37 +0300

    Migrate to Git LFS

---
+++ content/blog/en/en.11tydata.js
@@ -0,0 +1,16 @@
+module.exports = {
+	tags: ["posts"],
+	layout: "layouts/post.njk",
+	language: "en",
+	eleventyComputed: {
+		permalink: (data) => {
+			const date = data.date;
+			if (!date) return undefined;
+			const d = new Date(date);
+			const year = d.getUTCFullYear();
+			const month = String(d.getUTCMonth() + 1).padStart(2, "0");
+			const day = String(d.getUTCDate()).padStart(2, "0");
+			return `/blog/${year}/${month}/${day}/${data.page.fileSlug}/`;
+		},
+	},
+};
